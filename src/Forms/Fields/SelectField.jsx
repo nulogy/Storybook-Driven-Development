6183399b50dce26f1c0d2e876110214c75320867
Ned Schwartz <ned@theinterned.net>
2017-02-07 20:37:26 -0500

    extracts SelectField molecule component

---
+++ src/Forms/Fields/SelectField.jsx
@@ -0,0 +1,44 @@
+import React, { PropTypes } from "react";
+import Field from "./Field";
+
+const mapOptions = options => ( options.map(
+  ({text, value}) => <option value={value} key={value}>{ text ? text : value }</option>
+));
+
+const SelectField = ({
+  disabled = false,
+  label,
+  name,
+  options,
+  value,
+}) => (
+  <Field name={name} label={label}>
+    <select
+      disabled={disabled}
+      className="input inputSelect"
+      id={name}
+      name={name}
+      value={value}
+    >
+      { mapOptions(options) }
+    </select>
+  </Field>
+);
+
+SelectField.propTypes = {
+  disabled: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      text: PropTypes.string,
+    })
+  ).isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+}
+
+export default SelectField;
---
+++ src/Forms/Fields/SelectField.story.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import SelectField from './SelectField';
+
+export default function SelectFieldStory() {
+  const options = [
+    { value: "value_a", text: "Value A" },
+    { value: "value_b", text: "Value B" },
+    { value: "no_text_label" },
+  ];
+  const props = {
+    label: "a label",
+    name: "name",
+    options,
+  }
+
+  storiesOf('SelectField', module)
+    .add('default', () => (
+      <SelectField {...props} />
+    ))
+    .add('with a value', () => (
+      <SelectField {...props} value="value_b" />
+    ))
+    .add('disabled', () => (
+      <SelectField {...props} disabled />
+    ))
+    .add('disabled with a value', () => (
+      <SelectField {...props} value="value_b" disabled />
+    ))
+}
