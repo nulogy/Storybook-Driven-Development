cb9154ebca187dfc580600928231213cf607acaa
Ned Schwartz <ned@theinterned.net>
2017-02-07 21:26:20 -0500

    extracts common options

--- src/Intervals/Form/Form.jsx
+++ src/Intervals/Form/Form.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import TextField from "../../Forms/Fields/TextField";
 import SelectField from "../../Forms/Fields/SelectField";
 
+const options = [
+  { value: "plv_available", text: "PLV available" },
+  { value: "po_due", text: "PO due" }
+];
+
 const IntervalForm = props => (
   <form>
     <h2 className="title">Create new interval</h2>
 
-    <TextField name="interval_name" label="Add interval name" />
+    <TextField
+      name="interval_name"
+      label="Add interval name"
+    />
 
     <SelectField
       name="start_milestone"
       label="Select start milestone"
       value="plv_available"
-      options={[
-        { value: "plv_available", text: "PLV available" },
-        { value: "po_due", text: "PO due" }
-      ]}
+      options={options}
     />
 
     <SelectField
       name="end_milestone"
       label="Select end milestone"
       value="po_due"
-      options={[
-        { value: "plv_available", text: "PLV available" },
-        { value: "po_due", text: "PO due" }
-      ]}
+      options={options}
     />
 
     <div className="actions">
