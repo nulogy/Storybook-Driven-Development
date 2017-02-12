f2a22fe71a0ebd3074bdbabde43b5b347eea061d
Ned Schwartz <ned@theinterned.net>
2017-02-12 14:54:32 -0500

    adds background colours

--- src/Atoms/Colours.story.jsx
+++ src/Atoms/Colours.story.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import colours from './Colours.css';
 
+const textStyle = isInverse => isInverse ? {"backgroundColor": "black"} : null;
 const Text = ({className, inverse = false}) => (
   <p
     className={colours[className]}
-    style={inverse ? {"backgroundColor": "#444"} : null}
+    style={textStyle(inverse)}
   >{className}</p>);
+
+const boxStyle = isInverse => {
+  let style = {
+      "text-align": "center",
+      color: "white",
+      padding: "2rem",
+      height: "6rem",
+      "margin-bottom": "2rem"
+  };
+  if (isInverse === true) {
+    style = Object.assign(style, { color: "black"});
+  };
+  return style;
+};
+const Box = ({className, inverse = false}) => (
+  <div className={colours[className]} style={boxStyle(inverse)}>{className}</div>);
 
 export default function ColoursStory() {
   storiesOf('Colours', module)
@@ -19,5 +36,16 @@ export default function ColoursStory() {
         <Text className="colour__text--white" inverse />
         <Text className="colour__text--blue" />
       </div>
+    ))
+    .add('backgrounds', () => (
+      <div>
+        <Box className="colour__background--black" />
+        <Box className="colour__background--grey-dark" />
+        <Box className="colour__background--grey" />
+        <Box className="colour__background--grey-light" />
+        <Box className="colour__background--white" inverse />
+        <Box className="colour__background--blue" />
+      </div>
     ));
+
 }
