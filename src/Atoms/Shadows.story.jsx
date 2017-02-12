8a62d05d29288f0682b0f5625fcf78726fc6d868
Ned Schwartz <ned@theinterned.net>
2017-02-11 19:40:48 -0500

    adds shadow atoms

---
+++ src/Atoms/Shadows.story.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import shadows from './Shadows.css';
+
+const Box = ({className}) => (
+  <div className={shadows[className]} style={{
+      "text-align": "center",
+      padding: "2rem",
+      height: "6rem",
+      "margin-bottom": "2rem"
+  }}>{className}</div>);
+
+export default function ShadowsStory() {
+  storiesOf('Shadows', module)
+    .add('shadow styles', () => (
+      <div>
+        <Box className="one--light" />
+        <Box className="one--medium" />
+        <Box className="one--heavy" />
+
+        <Box className="two--light" />
+        <Box className="two--medium" />
+        <Box className="two--heavy" />
+
+        <Box className="three--light" />
+        <Box className="three--medium" />
+        <Box className="three--heavy" />
+      </div>
+    ))
+    .add('inset shadow styles', () => (
+      <div>
+        <Box className="one--inset--light" />
+        <Box className="one--inset--medium" />
+        <Box className="one--inset--heavy" />
+
+        <Box className="two--inset--light" />
+        <Box className="two--inset--medium" />
+        <Box className="two--inset--heavy" />
+
+        <Box className="three--inset--light" />
+        <Box className="three--inset--medium" />
+        <Box className="three--inset--heavy" />
+      </div>
+    ));
+}
