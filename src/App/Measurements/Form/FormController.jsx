7e5fe17ece47ea31a777b2c4edbe76234f4727a1
Ned Schwartz <ned@theinterned.net>
2017-02-13 21:26:38 -0500

    adds initial state and state passing

--- src/App/Measurements/Form/FormController.jsx
+++ src/App/Measurements/Form/FormController.jsx
@@ -1,13 +1,30 @@
 import React, { Component, PropTypes } from "react";
 
+function mapPropsToChildren(children, props) {
+  return React.Children.map(children, child => React.cloneElement(child, Object.assign(props, child.props)));
+}
+
 class FormController extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      interval_name: null,
+      start_milestone: null,
+      end_milestone: null
+    };
   }
-  
+
+
+  childProps() {
+    return Object.assign(
+      { handleChange: this.handleChange },
+      this.state
+    );
+  }
+
   render() {
     const { children } = this.props;
-    return <div>{ children }</div>
+    return (<div>{ mapPropsToChildren(children, this.childProps()) }</div>);
   }
 }
 
--- src/App/Measurements/Form/FormController.test.jsx
+++ src/App/Measurements/Form/FormController.test.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { shallow } from "enzyme";
+import keys from "lodash/keys";
 import FormController from "./FormController";
 
-const Mock = props => <span>{ props.children }</span>
-
-it("renders without crashing", () => {
-  expect(shallow(
-    <FormController />
-  ).find("div")).toBePresent();
-});
-
-it("renderes it's children", () => {
+describe("FormController", () => {
+  const Mock = props => <span>{ props.children }</span>
   const subject = shallow(
     <FormController>
       <Mock />
     </FormController>
   );
+  
+  describe("form values", () => {
+    const expectedFormNames = expect.arrayContaining([
+      "interval_name",
+      "start_milestone",
+      "end_milestone",
+    ]);
 
-  expect(subject.find("Mock")).toBePresent();
+    it("maintains form values in its state", () => {
+      expect(keys(subject.state())).toEqual(expectedFormNames);
+    });
+
+    it("passes required form values to children", () => {
+      expect(keys(subject.find("Mock").props()))
+        .toEqual(expectedFormNames);
+    });
+  });
+
 });
