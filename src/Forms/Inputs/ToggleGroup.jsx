11587ccf48e7ab0781388c0d62530f5149f89c14
Ned Schwartz <ned@theinterned.net>
2017-02-15 13:03:51 -0500

    implements ToggleGroup as a wrapper around RadioGroup

---
+++ src/Forms/Inputs/ToggleGroup.jsx
@@ -0,0 +1,10 @@
+import React, { PropTypes } from "react";
+import RadioGroup from "../Inputs/RadioGroup";
+
+const ToggleGroup = ({ ...radioProps }) => (
+  <RadioGroup {...radioProps} />
+)
+
+ToggleGroup.propTypes = {};
+
+export default ToggleGroup;
---
+++ src/Forms/Inputs/ToggleGroup.story.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import cloneDeep from "lodash/cloneDeep";
+import ToggleGroup from './ToggleGroup';
+
+export default function ToggleGroupStory() {
+  const options = [
+    { value: "value_a", text: "Value A" },
+    { value: "value_b", text: "Value B" },
+    { value: "no_text_label" },
+  ];
+
+  const disabledOptions = cloneDeep(options);
+  disabledOptions[1].disabled = true;
+
+  const props = {
+    name: "name",
+    options,
+    onChange: action("onChange"),
+  }
+
+  storiesOf('❤️ ToggleGroup', module)
+  .add('default', () => (
+    <ToggleGroup {...props} />
+  ))
+  .add('with a value', () => (
+    <ToggleGroup {...props} value="value_b" />
+  ))
+  .add('all disabled', () => (
+    <ToggleGroup {...props} disabled />
+  ))
+  .add('one disabled', () => (
+    <ToggleGroup {...props} options={disabledOptions} />
+  ))
+  .add('all disabled with a value', () => (
+    <ToggleGroup {...props} value="no_text_label" disabled />
+  ))
+  .add('one disabled with a value', () => (
+    <ToggleGroup {...props} value="no_text_label" options={disabledOptions} />
+  ));
+}
