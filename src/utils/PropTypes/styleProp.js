f487f7ef3e49338349c0b043b61798a90ba11dca
Ned Schwartz <ned@theinterned.net>
2017-02-15 14:41:43 -0500

    implements styleProp PropType validation

---
+++ src/utils/PropTypes/styleProp.js
@@ -0,0 +1,9 @@
+import { PropTypes } from "react";
+import mapValues from "lodash/mapValues";
+
+// Prop type validation that specifies the
+// class names in the passed ICSS object (https://github.com/css-modules/icss)
+// as the keys in a PropTypes.shape
+const styleProp = (icssStyles) => PropTypes.shape(mapValues(icssStyles, () => PropTypes.string))
+
+export default styleProp
