79832a11968ca12e76dcdc2446b5e77751f83754
Jason Kurian <jasonk92@gmail.com>
2017-02-16 15:51:54 -0500

    chore(lint): stylelint + stylelint-webpack-plugin

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -8,7 +8,7 @@ var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 var WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 var getClientEnvironment = require('./env');
 var paths = require('./paths');
-
+var StyleLintPlugin = require('stylelint-webpack-plugin');
 
 
 // Webpack uses `publicPath` to determine where the app is being served from.
@@ -201,7 +201,8 @@ module.exports = {
     // to restart the development server for Webpack to discover it. This plugin
     // makes the discovery automatic so you don't have to restart.
     // See https://github.com/facebookincubator/create-react-app/issues/186
-    new WatchMissingNodeModulesPlugin(paths.appNodeModules)
+    new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+    new StyleLintPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
