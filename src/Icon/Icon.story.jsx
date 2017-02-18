6d8cfd429ddf1b10722e2217ad0f8c9ef590bc34
Ned Schwartz <ned@theinterned.net>
2017-02-18 17:37:13 -0500

    a bit of storybook cleanup

--- src/Icon/Icon.story.jsx
+++ src/Icon/Icon.story.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Icon from './Icon';
+
+const Mount = ({ name }) => <p><Icon name={name} /> { name } Icon</p>;
+Mount.PropTypes = { name: PropTypes.string };
 
 export default function IconStory() {
   storiesOf('❤️ Icon', module)
     .add('default', () => (
       <div>
-        <Icon name="Milestone" />
-        <Icon name="Interval" />
+        <Mount name="Milestone" />
+        <Mount name="Interval" />
       </div>
     ))
-    .add('Icrease size by increasing parent size', () => (
+    .add('Size tracks surrounding text', () => (
       <div>
-        { [1, 2, 3, 4].map( size => (
-          <p style={{fontSize: `${size}em`}}>
-            font-size: {size}em
+        { [1, 2, 3, 4].map(size => (
+          <p style={{ fontSize: `${size}em` }}>
             <Icon name="Milestone" />
             <Icon name="Interval" />
+            &nbsp;
+            font-size: {size}em
           </p>
         )) }
       </div>
