fe9826a8f6a8d761c96718d6a347de8848e8044c
Ned Schwartz <ned@theinterned.net>
2017-02-18 16:34:08 -0500

    outputs simpler classnames in storybooks
    makes it easier to read and reason about the rendered markup

--- config/storybook.webpack.config.js
+++ config/storybook.webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.css$/,
-        loader: `style!css?importLoaders=1&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss`
+        loader: 'style!css?importLoaders=1&modules&localIdentName=[name]__[local]!postcss',
       },
       // "file" loader for svg
       {
