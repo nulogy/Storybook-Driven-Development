1b0d52e0a04812582fb264099aafdbc6edeb685d
Ned Schwartz <ned@theinterned.net>
2017-02-18 23:04:54 -0500

    adds Nav to App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import logo from './nulogy_logo.svg';
 import styles from './App.css';
+import Nav from "./Nav";
 import FormController from './Toggle/Form/FormController';
 import Form from './Toggle/Form';
 
 export default function App() {
   return (
-    <div>
-      <div className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <h1 className={styles.title}>Styleguide Driven Development</h1>
-      </div>
-      <div className={styles.content}>
+    <div className={styles.app}>
+      <Nav value="/edit" />
+      <main className={styles.content} role="main">
         <FormController>
           <Form />
         </FormController>
-      </div>
+      </main>
     </div>
   );
 }
