d7db35b2c2596a3f6a7dd7040fd7e544bde305b9
Ned Schwartz <ned@theinterned.net>
2017-02-18 19:58:43 -0500

    adds nav icons and updates story

--- src/Icon/Icon.story.jsx
+++ src/Icon/Icon.story.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { storiesOf } from '@kadira/storybook';
+import keys from "lodash/keys";
 import Icon from './Icon';
+import * as Icons from "./icons/";
 
 const Mount = ({ name, color = "inherit" }) => <p style={{ color }}><Icon name={name} /> { name } Icon</p>;
 Mount.PropTypes = {
@@ -12,18 +14,16 @@ export default function IconStory() {
   storiesOf('❤️ Icon', module)
     .add('default', () => (
       <div>
-        <Mount name="Milestone" />
-        <Mount name="Interval" />
+        { keys(Icons).map(icon => <Mount name={icon} />) }
       </div>
     ))
     .add('Size tracks surrounding text', () => (
       <div>
         { [1, 2, 3, 4].map(size => (
           <p style={{ fontSize: `${size}em` }}>
-            <Icon name="Milestone" />
-            <Icon name="Interval" />
+            @{size}em:
             &nbsp;
-            font-size: {size}em
+            { keys(Icons).map(icon => <Icon name={icon} />) }
           </p>
         )) }
       </div>
@@ -31,7 +31,9 @@ export default function IconStory() {
     .add('inherits parent colours', () => (
       <div>
         { ["red", "orange", "yellow", "green", "blue", "purple"].map(color =>
-          <Mount name="Interval" color={color} />
+          <div style={{color, fontSize: "4em"}}>
+            { keys(Icons).map(icon => <Icon name={icon} />) }
+          </div>
         )}
       </div>
     ));
