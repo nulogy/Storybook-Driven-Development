e5a8c61adac1ca12d9365352faa597b013944984
Ned Schwartz <ned@theinterned.net>
2017-02-19 16:36:54 -0500

    updates Icons story
    1. removes use of Mount component
    2. adds keys

--- src/Atoms/Icon/Icon.story.jsx
+++ src/Atoms/Icon/Icon.story.jsx
@@ -4,26 +4,20 @@ import keys from 'lodash/keys';
 import Icon from './Icon';
 import * as Icons from './icons/';
 
-const Mount = ({ name, color = 'inherit' }) => <p style={{ color }}><Icon name={name} /> { name } Icon</p>;
-Mount.propTypes = {
-  name: PropTypes.string.isRequired,
-  color: PropTypes.string,
-};
-
 export default function IconStory() {
   storiesOf('⭐️ Icon', module)
     .add('default', () => (
       <div>
-        { keys(Icons).map(icon => <Mount name={icon} />) }
+        { keys(Icons).map(icon => <p key={icon}><Icon name={icon} /> { icon } Icon</p>) }
       </div>
     ))
     .add('Size tracks surrounding text', () => (
       <div>
         { [1, 2, 3, 4].map(size => (
-          <p style={{ fontSize: `${size}em` }}>
+          <p style={{ fontSize: `${size}em` }} key={size}>
             @{size}em:
             &nbsp;
-            { keys(Icons).map(icon => <Icon name={icon} />) }
+            { keys(Icons).map(icon => <Icon name={icon} key={icon} />) }
           </p>
         )) }
       </div>
@@ -31,8 +25,8 @@ export default function IconStory() {
     .add('inherits parent colours', () => (
       <div>
         { ['red', 'orange', 'yellow', 'green', 'blue', 'purple'].map(color =>
-          <div style={{ color, fontSize: '4em' }}>
-            { keys(Icons).map(icon => <Icon name={icon} />) }
+          <div style={{ color, fontSize: '4em' }} key={color}>
+            { keys(Icons).map(icon => <Icon name={icon} key={icon} />) }
           </div>,
         )}
       </div>
