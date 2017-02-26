2d86bec9bf2ad9ba2a2a659822115148c3a03367
Ned Schwartz <ned@theinterned.net>
2017-02-26 11:18:52 -0500

    linting

--- config/storybook/StorybookTheme.jsx
+++ config/storybook/StorybookTheme.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { PropTypes } from 'react';
 import '~/index.css';
-import styles from "./StorybookTheme.css";
+import styles from './StorybookTheme.css';
 
-const StorybookTheme = ({children}) => (
+const StorybookTheme = ({ children }) => (
   <div className={styles.story}>
     {children}
   </div>
 );
+
+StorybookTheme.propTypes = { children: PropTypes.node };
 
 export default StorybookTheme;
