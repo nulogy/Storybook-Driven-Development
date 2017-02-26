06b54a37035bbae57faa94ed53e4b59019d82fff
Ned Schwartz <ned@theinterned.net>
2017-02-26 14:30:20 -0500

    proper padding for app layout

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './App.css';
 import Nav from './Nav';
-import FormController from './Toggle/Form/FormController';
-import Form from './Toggle/Form';
+import FormController from './Intervals/Form/FormController';
+import Form from './Intervals/Form';
 
 export default function App() {
   return (
