b5f8989d7a139ffe1ad3986c7878e4565bc7a664
Ned Schwartz <ned@theinterned.net>
2017-02-26 11:20:19 -0500

    Adds atomic design icons

--- src/Atoms/Icon/icons/index.js
+++ src/Atoms/Icon/icons/index.js
@@ -5,3 +5,8 @@ export { default as Interval } from './Interval.icon.svg';
 export { default as Milestone } from './Milestone.icon.svg';
 export { default as Portal } from './Portal.icon.svg';
 export { default as Table } from './Table.icon.svg';
+
+export { default as Quark } from './Quark.icon.svg';
+export { default as Atom } from './Atom.icon.svg';
+export { default as Molecule } from './Molecule.icon.svg';
+export { default as Organism } from './Organism.icon.svg';
